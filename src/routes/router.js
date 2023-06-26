c08a7e501faa9a88da4f1f0df10fa6debb0fa66b
RamiroFerradas <ramiferra97@gmail.com>
2023-06-26 12:40:49 -0300

    modularizacion de codigo

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,11 +1,10 @@
-require("dotenv").config();
 const express = require("express");
 const fs = require("fs");
 const { join } = require("path");
 const { createReadStream } = require("fs");
+const { verificarAutenticacion } = require("./auth");
 const router = express.Router();
 
-// Endpoint GET para obtener todos los menús
 router.get("/menus", (req, res) => {
   // Leer el archivo JSON con los menús
   fs.readFile("../chatbot/menus.json", "utf8", (err, data) => {
@@ -23,19 +22,9 @@ router.get("/menus", (req, res) => {
 });
 
 // Middleware para verificar la autenticación
-const verificarAutenticacion = (req, res, next) => {
-  const token = req.headers.authorization;
-
-  // Verifica si el token es válido
-  if (token === process.env.TOKEN) {
-    next();
-  } else {
-    res.status(401).send("Acceso no autorizado");
-  }
-};
 
 router.get("/get-qr", verificarAutenticacion, (req, res) => {
-  const YOUR_PATH_QR = join(process.cwd(), `../../bot.qr.png`);
+  const YOUR_PATH_QR = join(process.cwd(), `bot.qr.png`);
   const fileStream = createReadStream(YOUR_PATH_QR);
 
   res.writeHead(200, { "Content-Type": "image/png" });
